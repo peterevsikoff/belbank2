aaefebb0d899f71c2f630d278d285a2868ff9d92
peterevsikoff <93715088+peterevsikoff@users.noreply.github.com>
2021-11-28 23:37:07 +0200

    Add files via upload

---
+++ ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BelBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelBank.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> getAllProducts { get; set; }
+
+        public IEnumerable<Category> getAllCatigories { get; set; }
+        public string currentCategory { get; set; }
+
+        public IEnumerable<ProductWithCategory> getAllProductWithCategories { get; set; }
+    }
+}
